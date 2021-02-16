9ac712121d3e053ab7567e4157e90d816581fbf7
Adam Janicki <adamjanicki2@gmail.com>
2021-02-16 11:36:33 -0500

    silence of the lambs overflow fix

--- client/src/components/modules/BottomBar.js
+++ client/src/components/modules/BottomBar.js
@@ -23,6 +23,8 @@ class BottomBar extends Component {
             <div> • </div>
             <div onClick={()=>{navigate('/statistics')}} className='Bottom-links'>Stats</div>
             <div> • </div>
+            <a className="Bottom-links" href="mailto:adamoviessite@gmail.com" target="_blank">Contact</a>
+            <div> • </div>
             <a className="Bottom-links" href="/code.js" target="_blank">View Source Code</a>
         </div>
       );
